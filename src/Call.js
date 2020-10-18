ba3aacf9e432da39cebc585cd8de69f5b9915688
vigneshkarthikeyan <vignesh@alumni.purdue.edu>
2020-10-18 14:06:18 +0530

    added timer. Need to change the reactrap for better alignment of timer, the timer's styling

--- src/Call.js
+++ src/Call.js
@@ -9,7 +9,8 @@ import {
     FormGroup,
     Input,
     Label,
-    Row
+    Row,
+    Spinner
 } from "reactstrap";
 import React from 'react';
 import axios from "axios";
@@ -33,8 +34,56 @@ class Call extends React.Component {
 
     toggle = () => this.setState({dropdownOpen: !this.state.dropdownOpen})
 
+    callInfo = () => {
+        return (
+            <div>
+                <Row>
+                    <Col md={12}>
+                        <FormGroup>
+                            <Label for="Name">Name</Label>
+                            <Input type="text" id="Name" placeholder="Enter your full name"/>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={8}>
+                        <FormGroup>
+                            <Label for="PhoneNumber">Phone Number</Label>
+                            <Input type="number" id="PhoneNumber"
+                                   placeholder="Call recipient's 10 digit number. No extension"
+                                   required/>
+                        </FormGroup>
+                    </Col>
+                    <Col md={4}>
+                        <FormGroup>
+                            <Label for="phoneDropdown">Call Duration</Label>
+                            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} id="phoneDropdown">
+                                <DropdownToggle caret>
+                                    {this.state.selectedTime === 0 ? "Time" : this.state.selectedTime + " minutes"}
+                                </DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem header>Call Duration</DropdownItem>
+                                    <DropdownItem>
+                                        <div onClick={() => this.setState({selectedTime: 5})}>5</div>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <div onClick={() => this.setState({selectedTime: 10})}>10</div>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <div onClick={() => this.setState({selectedTime: 15})}>15</div>
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+
+    // todo extract component so that it can be passed a prop, loadPage, and will render either the timer or the entry form.
     render() {
-        if (this.state.loadPage === 1 && !this.state.loading) {
+        if (this.state.loadPage === 1) {
             return (
                 <div className="wrapper">
                     <Form className="login-form form-wrapper" onSubmit={(e) => this.startCall(e)}>
@@ -50,7 +99,8 @@ class Call extends React.Component {
                             <Col md={8}>
                                 <FormGroup>
                                     <Label for="PhoneNumber">Phone Number</Label>
-                                    <Input type="number" id="PhoneNumber" placeholder="Call recipient's 10 digit number. No extension"
+                                    <Input type="number" id="PhoneNumber"
+                                           placeholder="Call recipient's 10 digit number. No extension"
                                            required/>
                                 </FormGroup>
                             </Col>
@@ -77,7 +127,32 @@ class Call extends React.Component {
                                 </FormGroup>
                             </Col>
                         </Row>
-                        <Button color="success">Call</Button>
+                        <Button color="success">{!this.state.loading ? "Call" :
+                            <Spinner style={{width: '1rem', height: '1rem'}}/>}
+                        </Button>
+                    </Form>
+                </div>
+            )
+        } else {
+            return (
+                <div className="wrapper">
+                    <Form className="login-form form-wrapper">
+                        <Row>
+                            <Col md={{size: 6, offset: 4}}>
+                                <CountdownCircleTimer
+                                    isPlaying
+                                    duration={this.state.selectedTime * 60}
+                                    colors={[
+                                        ['#004777', 0.33],
+                                        ['#F7B801', 0.33],
+                                        ['#A30000', 0.33],
+                                    ]}
+                                >
+                                    {({remainingTime}) => remainingTime}
+                                </CountdownCircleTimer>
+                            </Col>
+                        </Row>
+                        <Button color="secondary" disabled>In Call</Button>
                     </Form>
                 </div>
             )
@@ -86,24 +161,21 @@ class Call extends React.Component {
 
     startCall(e) {
         e.preventDefault()
+
         this.setState({loading: true})
+        this.setState({loadPage: 2})
         let request = {
             name: document.getElementById('Name').value,
             phoneNumber: document.getElementById('PhoneNumber').value,
             callDuration: this.state.selectedTime,
         }
-        // axios.post('http://localhost:3000/auth/register', request)
-        //     .then(response => {
-        //         // stores jwt
-        //         auth.login(response.data)
-        //
-        //         if (auth.isAuthenticated(response.data)) {
-        //             navigate(`/call`)
-        //         }
-        //     })
-        //     .catch(err => {
-        //         console.log(err);
-        //     })
+        axios.post('http://localhost:3000/user/call', request)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 }
 
